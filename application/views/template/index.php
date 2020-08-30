e284dc022445089fb0e8d0321698c340ae96b46a
alieffebrina <alief.febrina@gmail.com>
2020-08-30 20:55:08 +0700

    welcome

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -22,7 +22,7 @@
           <!-- small box -->
           <div class="small-box bg-aqua">
             <div class="inner">
-              <h3><?php echo $barang; ?></h3>
+              <h3>15</h3>
 
               <p>Total Item</p>
             </div>
@@ -37,7 +37,7 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-              <h3><?php echo $datapelanggan; ?><sup style="font-size: 20px"></sup></h3>
+              <h3>15</h3>
 
               <p>Data Pelanggan</p>
             </div>
@@ -52,12 +52,9 @@
           <!-- small box -->
           <div class="small-box bg-yellow">
             <div class="inner">
-             <h3>Rp. <?php $total = 0; 
-              foreach ($totalpenjualan as $totalpenjualan) {
-                $total += $totalpenjualan->total;
-              }
-              echo number_format($total); ?></h3>
-              <p>Penjualan bulan <?php echo date('F Y')?></p>
+              <h3>25.000.000</h3>
+
+              <p>Penjualan Bulan <?php echo date('F Y')?></p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
@@ -84,130 +81,7 @@
       </div>
       <!-- /.row -->
       <!-- Main row -->
-      <div class="row">
-        <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
-          <!-- Custom tabs (Charts with tabs)-->
-          <!-- Bar chart -->
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <i class="fa fa-bar-chart-o"></i>
-
-              <h3 class="box-title">Grafik Penjualan</h3>
-
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
-            </div>
-            <div class="box-body">
-              <div id="bar-chart" style="height: 500px;"></div>
-            </div>
-            <!-- /.box-body-->
-          </div>
-          <!-- /.nav-tabs-custom -->
-          <!-- /.box -->
-        </section>
-        <!-- /.Left col -->
-        <!-- right col (We are only adding the ID to make the widgets sortable)-->
-        <section class="col-lg-5 connectedSortable">
-           <div class="box box-solid bg-green-gradient">
-            <div class="box-header">
-              <i class="fa fa-calendar"></i>
-
-              <h3 class="box-title">Stok Limit</h3>
-              <!-- tools box -->
-              <div class="pull-right box-tools">
-                <!-- button with a dropdown -->
-                <div class="btn-group">
-                  <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
-                    <i class="fa fa-bars"></i></button>
-                  <ul class="dropdown-menu pull-right" role="menu">
-                    <li><a href="#">Add new event</a></li>
-                    <li><a href="#">Clear events</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">View calendar</a></li>
-                  </ul>
-                </div>
-                <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-success btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                </button>
-              </div>
-              <!-- /. tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
-              <!--The calendar -->
-              <div id="calendar" style="width: 100%"></div>
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer text-black">
-              <div class="row">
-                <?php foreach ($stokdashboard as $key) { 
-                  $a = 100-(($key->stok/$key->stokmin)*100);?>
-
-                <div class="col-sm-12">
-                  <!-- Progress bars -->
-                  <div class="clearfix">
-                    <span class="pull-left"><?php echo $key->barang.' / '.$key->jenisbarang; ?></span>
-                    <small class="pull-right"><?php echo $key->stok.' / '.$key->satuan ?></small>
-                  </div>
-                  <div class="progress xs">
-                    <div class="progress-bar progress-bar-green" style="width: <?php echo $a ?>%;"></div>
-                  </div>
-                </div>
-
-                <?php } ?>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
-            </div>
-          </div>
-          <!-- Map box -->
-           <div class="box box-primary">
-            <div class="box-header">
-              <i class="ion ion-clipboard"></i>
-
-              <h3 class="box-title">Piutang</h3>
-
-              <div class="box-tools pull-right">
-                <ul class="pagination pagination-sm inline">
-                  <li><a href="#">&laquo;</a></li>
-                  <li><a href="#">1</a></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">&raquo;</a></li>
-                </ul>
-              </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <!-- See dist/js/pages/dashboard.js to activate the todoList plugin -->
-              <ul class="todo-list">
-                <?php foreach ($hutangdashboard as $hd) { ?>
-                <li>
-                  <!-- drag handle -->
-                  <span class="handle">
-                        <i class="fa fa-ellipsis-v"></i>
-                        <i class="fa fa-ellipsis-v"></i>
-                      </span>
-                  <!-- checkbox -->
-                  <input type="checkbox" value="">
-                  <!-- todo text -->
-                  <span class="text"><?php echo $hd->nama ?></span>
-                  <!-- Emphasis label -->
-                  <small class="label label-danger"><i class="fa fa-clock-o"></i> Rp. <?php echo number_format($hd->totalhutang) ?></small>
-                </li>
-                <?php } ?>
-              </ul>
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer clearfix no-border">
-              <a href="<?php echo site_url('C_penjualan/hutang'); ?>"><button type="button" class="btn btn-default pull-right">Selengkapnya</button>
-            </div>
-          </div>
+      
           <!-- /.box -->
 
           <!-- solid sales graph -->
