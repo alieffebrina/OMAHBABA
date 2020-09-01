4793aa61c974476e3117d1601e5f3a7040a8b791
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-09-01 23:29:00 +0700

    confirm hapus

--- application/views/master/satuan/v_addsatuan.php
+++ application/views/master/satuan/v_addsatuan.php
@@ -95,7 +95,7 @@
                     <div class="btn-group">
                       <a href="<?php echo site_url('C_satuan/view/'.$satuan->id_satuan); ?>"><button type="button" class="btn btn-success"><i class="fa fa-fw fa-search"></i></button></a>
                       <a href="<?php echo site_url('C_satuan/edit/'.$satuan->id_satuan); ?>"><button type="button" class="btn btn-info"><i class="fa fa-fw fa-pencil-square-o"></i></button></a>
-                      <a href="<?php echo site_url('C_satuan/hapus/'.$satuan->id_satuan); ?>"><button type="button" class="btn btn-danger"><i class="fa fa-fw fa-trash-o"></i></button></a>
+                      <a href="<?php echo site_url('C_satuan/hapus/'.$satuan->id_satuan); ?>" onclick="return confirm('Yakin Hapus?')"><button type="button" class="btn btn-danger"><i class="fa fa-fw fa-trash-o"></i></button></a>
                     </div>
                   </td>
                 </tr>
