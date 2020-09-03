5246dd7ca551ecbbf8254e1c23391f8ceee22751
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-09-03 14:12:22 +0700

    update tombol action

--- application/views/master/cabang/v_cabang.php
+++ application/views/master/cabang/v_cabang.php
@@ -61,9 +61,9 @@
                   <td><?php echo $cabang->email;?></td>
                   <td>
                     <div class="btn-group">
-                      <a href="<?php echo site_url('C_cabang/view/'.$cabang->id_cabang); ?>"><button type="button" class="btn btn-success">Lihat</button></a>
-                      <a href="<?php echo site_url('C_cabang/edit/'.$cabang->id_cabang); ?>"><button type="button" class="btn btn-info">Edit</button></a>
-                      <a href="<?php echo site_url('C_cabang/hapus/'.$cabang->id_cabang); ?>" onclick="return confirm('Yakin Hapus?')"><button type="button" class="btn btn-danger">Hapus</button></a>
+                       <a href="<?php echo site_url('C_cabang/view/'.$cabang->id_cabang); ?>"><button type="button" class="btn btn-success"><i class="fa fa-fw fa-search"></i></button></a>
+                      <a href="<?php echo site_url('C_cabang/edit/'.$cabang->id_cabang); ?>"><button type="button" class="btn btn-info"><i class="fa fa-fw fa-pencil-square-o"></i></button></a>
+                      <a href="<?php echo site_url('C_cabang/hapus/'.$cabang->id_cabang); ?>" onclick="return confirm('Yakin Hapus?')"><button type="button" class="btn btn-danger"><i class="fa fa-fw fa-trash-o"></i></button></a>
                     </div>
                   </td>
                 </tr>
